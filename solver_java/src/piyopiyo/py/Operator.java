db660cb15fc9c30f06a0209dd6c770532bdbd024
Yusuke Izumi <yuizumi@y5i.org>
2013-08-09 20:26:58 +0900

    Maps from Operator to {Unary,Binary}ExpressionFactory.

--- solver_java/src/piyopiyo/py/Operator.java
+++ solver_java/src/piyopiyo/py/Operator.java
@@ -1,7 +1,26 @@
 package piyopiyo.py;
 
+import java.util.Map;
+import com.google.common.collect.ImmutableMap;
+import piyopiyo.py.expressions.*;
+
 public enum Operator {
     not, shl1, shr1, shr4, shr16,
     and, or, xor, plus,
-    if0, fold, tfold
+    if0, fold, tfold;
+
+    public static final Map<Operator, UnaryExpressionFactory> UNARY_OPERATORS =
+        ImmutableMap.<Operator, UnaryExpressionFactory> builder()
+            .put(not, Not.FACTORY)
+            .put(shl1, Shl1.FACTORY)
+            .put(shr1, Shr1.FACTORY)
+            .put(shr4, Shr4.FACTORY)
+            .put(shr16, Shr16.FACTORY).build();
+
+    public static final Map<Operator, BinaryExpressionFactory> BINARY_OPERATORS =
+        ImmutableMap.<Operator, BinaryExpressionFactory> builder()
+            .put(and, And.FACTORY)
+            .put(or, Or.FACTORY)
+            .put(xor, Xor.FACTORY)
+            .put(plus, Plus.FACTORY).build();
 }
