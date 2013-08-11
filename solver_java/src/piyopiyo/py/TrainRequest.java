7153e66e9bfdbf024f32daefac427fe55297da6c
Nobuki Yoda <nobuki.y@gmail.com>
2013-08-11 18:53:48 +0900

    Fixed the type of TrainRequest.operators.

--- solver_java/src/piyopiyo/py/TrainRequest.java
+++ solver_java/src/piyopiyo/py/TrainRequest.java
@@ -1,14 +1,11 @@
 package piyopiyo.py;
 
 public class TrainRequest {
-	public TrainRequest(int size, Operators operators)
+	public TrainRequest(int size, Operator[] operators)
 	{
 		this.size = size;
 		this.operators = operators;
 	}
 	public int size;
-	public Operators operators;
-	public enum Operators{
-		fold, tfold
-	}
+	public Operator[] operators;
 }
