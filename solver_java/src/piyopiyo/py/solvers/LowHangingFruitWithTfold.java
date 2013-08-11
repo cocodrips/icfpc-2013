3d0a423aa64bff440f9e2c47e1269cd341ecef54
Nobuki Yoda <nobuki.y@gmail.com>
2013-08-11 13:08:16 +0900

    Updated low hanging fruit solver.

--- solver_java/src/piyopiyo/py/solvers/LowHangingFruitWithTfold.java
+++ solver_java/src/piyopiyo/py/solvers/LowHangingFruitWithTfold.java
@@ -6,26 +6,28 @@ import java.util.List;
 
 import piyopiyo.py.Operator;
 import piyopiyo.py.Problem;
+import piyopiyo.py.expressions.Constant;
 import piyopiyo.py.expressions.Expression;
+import piyopiyo.py.expressions.Fold;
 import piyopiyo.py.expressions.Program;
 import piyopiyo.py.expressions.Variable;
 import piyopiyo.py.skeltons.Skelton;
 
 import com.google.common.collect.ImmutableList;
 
-public class LowHangingFruitWithoutTfold extends SimpleSolver {
-    public static final LowHangingFruitWithoutTfold SOLVER =
-            new LowHangingFruitWithoutTfold();
+public class LowHangingFruitWithTfold extends SimpleSolver {
+    public static final LowHangingFruitWithTfold SOLVER =
+            new LowHangingFruitWithTfold();
     private static final int SIZE_TO_TRY = 7;
-    private LowHangingFruitWithoutTfold() {}
+    private LowHangingFruitWithTfold() {}
 
     @Override
     public boolean canSolve(Problem problem) {
         List<Operator> ops = Arrays.asList(problem.operators);
-        return (problem.size >= 10 &&
-                !ops.contains(Operator.tfold) &&
+        return (problem.size >= 14 &&
+                ops.contains(Operator.tfold) &&
                 !ops.contains(Operator.plus) &&
-                !ops.contains(Operator.fold)); 
+                !ops.contains(Operator.fold));
     }
 
     @Override
@@ -36,11 +38,13 @@ public class LowHangingFruitWithoutTfold extends SimpleSolver {
 
         List<Operator> ops = Arrays.asList(problem.operators);
         Variable x = new Variable("x");
+        Variable x0 = new Variable("x0");
+        Variable y0 = new Variable("y0");
         List<Variable> vars = ImmutableList.of(x);
         for (List<Skelton> skeltons : skeltonLists) {
             for (Skelton skelton : skeltons) {
                 for (Expression e : skelton.buildExpressions(ops, vars)) {
-                    programs.add(new Program(x, e));
+                    programs.add(new Program(x, new Fold(x, Constant.ZERO, x0, y0, e)));
                 }
             }
         }
